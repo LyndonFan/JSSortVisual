d3652aa76e5494ae355d61034d4547addb9022e1
LyndonFan <lyndon0808@gmail.com>
2020-11-29 00:37:24 +0800

    add speed up/down

--- SortingVisualizer.js
+++ SortingVisualizer.js
@@ -2,18 +2,24 @@ var list;
 var events = [];
 var prevEvents = [];
 var isSorting = false;
+var autoPlay = true;
+const updateTimes = [2000,1000,500,250,100,50];
+let timeSlider;
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   textAlign(CENTER);
-  var listLength = random(6,12);
+  var listLength = random(20,40);
   var tempList = [];
   for (var i = 0; i < listLength; i++){
-    var v = round(random(1,20));
+    var v = round(random(1,50));
     tempList.push(v);
     print(tempList[i]);
   }
   list = new Blocks(tempList);
+  timeSlider = createSlider(0,updateTimes.length-1,updateTimes.indexOf(500),1);
+  timeSlider.position(100, height-25);
+  timeSlider.style('width',width-200+'px');
 }
 
 function valToColor(prop, s){
@@ -48,20 +54,32 @@ async function updateList(){
     if (tup.length>1){list.updateActive(tup[1]);}
     if (indices.length==2){swap(list.vals,indices[0],indices[1]);}
     prevEvents.push(tup);
-    setTimeout(updateList,1000);
+    if (autoPlay) {setTimeout(updateList,updateTimes[timeSlider.value()]);}
   } else {
     list.resetActive();
   }
 }
 
 function keyPressed() {
-  if (!isSorting && keyCode === 32) {
-    var sorter = new Sorter(quickSort);
-    sorter.sort();
+  if (!isSorting && events.length==0){
+    switch (keyCode){
+      case 81: {sortWith(naiveQuickSort); break;} //"Q" -- naive Quicksort
+      case 82: {sortWith(shuffleList); break;}    //"R" -- Restart
+      case 83: {sortWith(selectionSort); break;}  //"S" -- Selectionsort
+      case 71: {sortWith(bogoSort); break;}       //"G" -- boGosort
+      case 66: {sortWith(bubbleSort); break;}     //"B" -- Bubblesort
+      case 72: {sortWith(heapSort); break;}       //"H" -- Heapsort
+      case 77: {sortWith(mergeSort); break;}      //"M" -- Mergesort
+      case 73: {sortWith(insertionSort); break;}  //"I" -- Insertionsort
+      case 80: {sortWith(quickSort); break;}      //"P" -- quicksort with smart Pivot
+      default: {break;}
+    }
   }
-  if (!isSorting && keyCode === 83) {
-    var sorter = new Sorter(shuffleList);
-    sorter.sort();
+  if (keyCode == 32){                             //" " -- autoPlay on/off
+    autoPlay = !autoPlay; print(autoPlay); updateList();
+  }
+  else if (keyCode == 39){                        //"→" -- autoPlay off, next step
+    autoPlay = false; updateList();
   }
 }
 
@@ -71,4 +89,6 @@ function draw() {
   stroke(0);
   strokeWeight(2);
   list.show();
+  timeSlider.position(100, height-30);
+  timeSlider.style('width',width-200+'px');
 }
