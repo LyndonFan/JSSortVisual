7849e316d69ee2f7d3fcfb9c4f0a5b158e178e8f
LyndonFan <lyndon0808@gmail.com>
2020-11-29 00:37:14 +0800

    change class to function

--- Sorter.js
+++ Sorter.js
@@ -1,19 +1,33 @@
-class Sorter{
-	constructor(sortFunction){
-		this.sortFunction = sortFunction;
-	}
+// class Sorter{
+// 	constructor(sortFunction){
+// 		this.sortFunction = sortFunction;
+// 	}
 
-	sort(){
-		isSorting = true;
-		if (list.len() <= 1){isSorting = false; return;}
-		print("Begin sorting...");
-		var lis = [];
-		for (var i = 0; i<list.len(); i++){
-			lis.push(list.vals[i]);
-		}
-		events = this.sortFunction(lis);
-		print("Done sorting.");
-		isSorting = false;
-		updateList();
+// 	sort(){
+// 		isSorting = true;
+// 		if (list.len() <= 1){isSorting = false; return;}
+// 		print("Begin sorting...");
+// 		var lis = [];
+// 		for (var i = 0; i<list.len(); i++){
+// 			lis.push(list.vals[i]);
+// 		}
+// 		events = this.sortFunction(lis);
+// 		print("Done sorting.");
+// 		isSorting = false;
+// 		updateList();
+// 	}
+// }
+
+function sortWith(sortFunction){
+	isSorting = true;
+	if (list.len() <= 1){isSorting = false; return;}
+	print("Begin sorting...");
+	var lis = [];
+	for (var i = 0; i<list.len(); i++){
+		lis.push(list.vals[i]);
 	}
+	events = sortFunction(lis);
+	print("Done sorting.");
+	isSorting = false;
+	updateList();
 }
